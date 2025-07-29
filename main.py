1eb7d72c14c35a07ea8f601cd7fea57e98a60b7b
TakiScript <takiscript@gmail.com>
2025-07-30 03:03:54 +0600

    Check " " in user input

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+def validate_input(user_input):
+  try:
+    if " " in user_input:
+      return True
+    else:
+      return False
+  except:
+    print("Invalid input.")
+
+
+
 def main():
   while True: 
     user_input: str = input("github-activity > ")
     
     print(f'You said: {user_input}')
-    
+    print(f"{validate_input(user_input)}")
     
 if __name__ == "__main__":
   main()
