f5231a779beb5a4e65c1622e9322fbafdee74cb7
TakiScript <takiscript@gmail.com>
2025-08-02 00:13:35 +0600

    Separately print all events

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
 import requests
 import json
 
-url: str = "https://api.github.com/users/TakiMoretti/events/public"
+base_url: str = "https://api.github.com/users/{username}/events/public"
 
-response = requests.get(url)
-first_five: dict = response.json()[0:5]
-
-pretty: str = json.dumps(first_five, indent=4)
-
-events: dict = first_five
-
-for event in events:
+def fetch_data(username: str):
+  final_url: str = base_url.format(username=username)
+  response = requests.get(final_url)
+  first_five: list = response.json()[0:5]
   
-  type: str = event["type"]
-  print("Event type is: {}".format(type))
-
-
-"""
+  pretty: str = json.dumps(first_five, indent=4)
+  
+  events: list = first_five
+  
+  for event in events:
+     type: str = event.get("type", "N/A")
+    
+     if type == "PushEvent":
+       print(f"-  A commit has been pushed to a repository.")
+     elif type == "CreateEvent":
+       print(f"-  A branch or tag has been created.")
+     elif type == "ForkEvent":
+       print(f"-  A repository has been forked.")
+     elif type == "WatchEvent":
+       print(f"-  A user has starred a repository.")
+     elif type == "PullRequestEvent":
+       print(f"-  A pull request has been opened, closed, or merged.")
+     elif type == "IssuesEvent":
+       print(f"-  An issue has been opened, closed, or reopened.")
+     elif type == "IssueCommentEvent":
+       print(f"-  A comment has been added to an issue or pull request.")
+     else:
+       print(f"Unknown type: {type}")
+  
 def validate_input(user_input):
   try:
     if " " in user_input.strip():
-      return True
-    else:
       return False
+    else:
+      return True
   except:
     print("Invalid input.")
 
@@ -32,8 +47,12 @@ def main():
     user_input: str = input("github-activity > ")
     
     print(f'You said: {user_input}')
-    print(f"{validate_input(user_input)}")
+    
+    validate_input(user_input)
+    
+    username: str = user_input.strip()
+    
+    fetch_data(username)
     
 if __name__ == "__main__":
   main()
-"""
