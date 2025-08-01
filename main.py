9fe2badb174f55e638e972e358486d8c64e6dd05
TakiScript <takiscript@gmail.com>
2025-08-01 23:38:40 +0600

    Fetch 5 events and print the type

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+import requests
+import json
+
+url: str = "https://api.github.com/users/TakiMoretti/events/public"
+
+response = requests.get(url)
+first_five: dict = response.json()[0:5]
+
+pretty: str = json.dumps(first_five, indent=4)
+
+events: dict = first_five
+
+for event in events:
+  
+  type: str = event["type"]
+  print("Event type is: {}".format(type))
+
+
+"""
 def validate_input(user_input):
   try:
-    if " " in user_input:
+    if " " in user_input.strip():
       return True
     else:
       return False
   except:
     print("Invalid input.")
-
 
 
 def main():
@@ -18,3 +36,4 @@ def main():
     
 if __name__ == "__main__":
   main()
+"""
